2da947d144772eb09b0391a2000568efc1095600
Manopsita <manopsitareddy@gmail.com>
2023-03-02 17:41:33 +0530

    #1 Module Based app

---
+++ projects/standalone/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { CommonModule } from "@angular/common";
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
+  providers: [],
+})
+export class AppComponent {
+  title = "standalone";
+}
