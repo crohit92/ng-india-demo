2c8bedafb2213b9a4d014b0f166049358ccace28
Manopsita <manopsitareddy@gmail.com>
2023-03-02 17:44:52 +0530

    #2 standalone component

--- projects/standalone/src/app/app.component.ts
+++ projects/standalone/src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { CommonModule } from "@angular/common";
+import { CommonModule, NgSwitch, NgSwitchCase } from "@angular/common";
 import { Component } from "@angular/core";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.scss"],
+  standalone: true,
   providers: [],
 })
 export class AppComponent {
