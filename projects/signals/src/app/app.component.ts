78cb937e831074aed4951b60eb7cd5b3f6df5845
Rohit Chopra <crohit92@gmail.com>
2023-03-05 15:30:38 +0530

    #9 Signals should be syncronous, otherwise then won't work as expected.

--- projects/signals/src/app/app.component.ts
+++ projects/signals/src/app/app.component.ts
@@ -10,15 +10,27 @@ export class AppComponent {
   title = "signals";
 
   counter: SettableSignal<number>;
+  user: SettableSignal<string>;
 
   constructor() {
     this.counter = signal(0);
+    this.user = signal("Rohit");
+
+    // This effect does not fire on change to the signal inside it.
     effect(() => {
-      console.log("Count changed", this.counter());
+      Promise.resolve().then(() => {
+        console.log("Count changed", this.counter());
+      });
+    });
+    effect(() => {
+      console.log("Name changed", this.user());
     });
 
     setInterval(() => {
       this.counter.set(this.counter() + 1);
+    }, 1000);
+    setInterval(() => {
+      this.user.set("Rohit" + this.counter());
     }, 1000);
   }
 }
